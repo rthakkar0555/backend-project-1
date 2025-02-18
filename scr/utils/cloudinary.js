9d258970fb79c027ad21bcf135dda1f0e589adb3
RIshi Thakkar <rishithakkar0555@gmail.com>
2025-02-18 20:10:35 +0530

    error showing fix

--- scr/utils/cloudinary.js
+++ scr/utils/cloudinary.js
@@ -1,16 +1,16 @@
 import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
-import { v2 as cloudinary } from 'cloudinary';
-
 cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_CLOUD_API_KEY, 
-  api_secret: process.env.CLOUDINARY_CLOUD_SECREAT 
+  cloud_name: `${process.env.CLOUDINARY_CLOUD_NAME}`, 
+  api_key: `${process.env.CLOUDINARY_CLOUD_API_KEY}`, 
+  api_secret: `${process.env.CLOUDINARY_CLOUD_SECREAT}`
 });
 
 const uploadONCloudinary= async(localpath)=>{
   try{
+    console.log(process.env.CLOUDINARY_CLOUD_NAME,process.env.CLOUDINARY_CLOUD_API_KEY,process.env.CLOUDINARY_CLOUD_SECREAT )
+    console.log(localpath)
     if(!localpath) return null
     const respons=await cloudinary.uploader.upload(localpath,{
       resource_type:"auto"
@@ -19,6 +19,7 @@ const uploadONCloudinary= async(localpath)=>{
     return respons;
   }
   catch(error){
+    console.log(error)
     fs.unlinkSync(localpath) // remove locally if fails
     return null
   }
